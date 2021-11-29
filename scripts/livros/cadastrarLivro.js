08a89ddb150607be0bb0ade4d162f5691bd79028
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2021-11-29 13:22:14 -0300

    Adicionandoa tag p para requirido em livros

--- scripts/livros/cadastrarLivro.js
+++ scripts/livros/cadastrarLivro.js
@@ -30,4 +30,46 @@ $(document).ready(function() {
             }
         })
     })
+
+    function validar(){
+        let salvar = true;
+        if(document.getElementById("titulo").value === ""){
+            document.getElementById("reqNome").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementsByClassName("emails")[0].value === ""){
+            document.getElementById("reqEmail").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("dataNascimento").value === ""){
+            document.getElementById("reqData").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("endereco").value === ""){
+            document.getElementById("reqEnd").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("cpf").value === ""){
+            document.getElementById("reqCpf").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementsByClassName("telefones")[0].value === ""){
+            document.getElementById("reqTel").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("profissao").value === ""){
+            document.getElementById("reqProf").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("numero").value === ""){
+            document.getElementById("reqNum").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+        if(document.getElementById("cep").value === ""){
+            document.getElementById("reqCep").innerHTML = "Este campo é requerido."
+            salvar = false;
+        }
+
+        return salvar;
+    }
 })
